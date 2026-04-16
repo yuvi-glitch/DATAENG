46eb83a25751f29fe663c6bec0b3421050054e9d
yuvi-glitch <84839760+yuvi-glitch@users.noreply.github.com>
2026-04-16 23:31:02 +0530

    Update stratascratch_solutions.sql

--- sql-practice/interview-problems/stratascratch_solutions.sql
+++ sql-practice/interview-problems/stratascratch_solutions.sql
@@ -6,6 +6,7 @@
 -- Mistake made: Used COUNT instead of SUM for n_messages
 -- =====================================================
 
+-- solution 
 WITH cte AS (
     SELECT 
         id_guest,
@@ -19,3 +20,17 @@ SELECT
     total_messages
 FROM cte
 ORDER BY rnk;
+
+
+
+-- 2nd problem 
+-- Number Of Units Per Nationality
+-- Write a query that returns how many different apartment-type units (counted by distinct unit_id) are owned by people under 30,
+-- grouped by their nationality. Sort the results by the number of apartments in descending order.
+
+-- solution 
+select count(distinct unit_id),h.nationality from  
+airbnb_units u join airbnb_hosts h on h.host_id = u.host_id
+where age < 30 AND u.unit_type = 'Apartment'
+group by h.nationality
+order by count(unit_id) desc
