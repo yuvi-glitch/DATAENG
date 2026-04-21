09cf235ad66eb1f33778f7ba9cd5fc9bc9a1a059
yuvi-glitch <84839760+yuvi-glitch@users.noreply.github.com>
2026-04-21 22:46:09 +0530

    Update stratascratch_solutions.sql

--- sql-practice/interview-problems/stratascratch_solutions.sql
+++ sql-practice/interview-problems/stratascratch_solutions.sql
@@ -34,3 +34,21 @@ airbnb_units u join airbnb_hosts h on h.host_id = u.host_id
 where age < 30 AND u.unit_type = 'Apartment'
 group by h.nationality
 order by count(unit_id) desc
+
+-- Finding User Purchases
+-- Identify returning active users by finding users who made a second purchase within 1 to 7 days after their first purchase. 
+-- Ignore same-day purchases. Output a list of these user_ids.
+select user_id from (select user_id, purchase_date,
+row_number () over ( partition by user_id order by date(purchase_date)) as rn from 
+(
+select distinct user_id ,
+date(created_at) as purchase_date 
+from amazon_transactions 
+) as distinct_dates)  as numbered 
+group by user_id 
+HAVING 
+    COUNT(*) >= 2
+    AND DATEDIFF(
+      MAX(CASE WHEN rn = 2 THEN purchase_date END),
+      MIN(CASE WHEN rn = 1 THEN purchase_date END)
+    ) BETWEEN 1 AND 7
