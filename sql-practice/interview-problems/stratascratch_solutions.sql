69e42c93cd4d9c16c3413931cd11e5f5a2f2408a
yuvi-glitch <84839760+yuvi-glitch@users.noreply.github.com>
2026-04-25 17:40:49 +0530

    Update stratascratch_solutions.sql

--- sql-practice/interview-problems/stratascratch_solutions.sql
+++ sql-practice/interview-problems/stratascratch_solutions.sql
@@ -103,3 +103,27 @@ FROM total_compensation
 GROUP BY employee_title, sex;
 
 
+/*You are given a set of projects and employee data. Each project has a name, a budget, and a specific duration, while each employee has an annual salary and may be assigned to one or more projects for particular periods. 
+The task is to identify which projects are overbudget. A project is considered overbudget if the prorated cost of all employees assigned to it exceeds the project’s budget.
+To solve this, you must prorate each employee's annual salary based on the exact period they work on a given project, relative to a full year. 
+For example, if an employee works on a six-month project, only half of their annual salary should be attributed to that project. 
+Sum these prorated salary amounts for all employees assigned to a project and compare the total with the project’s budget.
+Your output should be a list of overbudget projects, where each entry includes the project’s name, its budget, and the total prorated employee expenses for that project. 
+The total expenses should be rounded up to the nearest dollar. Assume all years have 365 days and disregard leap years.*/
+
+SELECT a.title,
+  a.budget,
+CEILING(DATEDIFF(a.end_date, a.start_date) *  SUM(c.salary) / 365) AS prorated_employee_expense
+
+FROM linkedin_projects a
+INNER JOIN linkedin_emp_projects b ON a.id = b.project_id
+INNER JOIN linkedin_employees c ON b.emp_id = c.id
+GROUP BY a.title,
+    a.budget,
+    a.end_date,
+    a.start_date
+HAVING CEILING(DATEDIFF(a.end_date, a.start_date) * 
+  SUM(c.salary) / 365) > a.budget
+ORDER BY a.title ASC;
+
+
